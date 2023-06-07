da1eca5117a3267208cc7d0549e710c6a9c3a162
edwin <edwin.lingson@gmail.com>
2023-06-07 10:47:26 -0400

    Ex 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const replaced = word.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    // console.log(replaced);
+    const reversedWord = replaced.split('').reverse().join('');
+    if(replaced === reversedWord) return true;
+    return false;
+   
 };
-
 // Do not edit below this line
 module.exports = palindromes;
