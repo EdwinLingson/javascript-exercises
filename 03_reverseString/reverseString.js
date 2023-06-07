44c02461bbe0cc615c6901b1b958d83b43f7f210
edwin <edwin.lingson@gmail.com>
2023-06-07 10:17:28 -0400

    Ex 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function(word) {
 
+    const splitWords = word.split('');
+    const revChars = splitWords.reverse();
+    return revChars.join('')
 };
 
 // Do not edit below this line
