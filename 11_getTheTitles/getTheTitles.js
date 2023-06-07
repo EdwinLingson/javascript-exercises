28e8a311ae4a57a0b8c5d75778a86d0387ece663
edwin <edwin.lingson@gmail.com>
2023-06-07 10:59:16 -0400

    Ex 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,8 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
-};
-
+    return books.map(e =>e.title)
+      
+    };
+    
 // Do not edit below this line
 module.exports = getTheTitles;
