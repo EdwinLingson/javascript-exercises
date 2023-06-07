a3e6c4d26c87295ec3783f24a879c2c770b3dca3
edwin <edwin.lingson@gmail.com>
2023-06-07 10:24:14 -0400

    Ex 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,...val) {
+    val.forEach(element => 
+        {const index = array.indexOf(element);
+        if(index>-1)  array.splice(index,1);});
+    
+    return array;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
