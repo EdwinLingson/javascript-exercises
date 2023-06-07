09b61cfb1da3e107a35e39cebaf65e1f1b27000c
edwin <edwin.lingson@gmail.com>
2023-06-07 11:01:14 -0400

    Ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    people.map(e =>{
+        e.yearOfDeath = e.yearOfDeath ===undefined?2023:e.yearOfDeath;
+    })
+    const Oldest = people.reduce(
+        (accumulator, currentValue) => 
+        
+        {
+          return accumulator.yearOfDeath - accumulator.yearOfBirth > currentValue.yearOfDeath - currentValue.yearOfBirth ? accumulator : currentValue},
+        people[0].yearOfDeath - people[0].yearOfBirth
+      );
+      return Oldest;
 
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
