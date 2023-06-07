90e8fac05dfe699ecd5645309749fd254d93c626
edwin <edwin.lingson@gmail.com>
2023-06-07 10:44:17 -0400

    Ex 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a -b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum =0;
+  array.forEach(a=> {
+    sum  = sum +a;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let sum =1;
+  array.forEach(a=> {
+    sum  = sum *a;
+  });
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if(n<2) return 1;
+  return n * factorial(n-1);
 };
 
 // Do not edit below this line
