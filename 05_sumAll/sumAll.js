5fbfbb729c036097e58cc2772efff2d2d338b8fb
edwin <edwin.lingson@gmail.com>
2023-06-07 10:25:12 -0400

    Ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if(start<0 || end <0) return "ERROR";
+    if(typeof start !='number' || typeof end != "number") return "ERROR";
+const n1 = (start *(start+1))/2;
+const n2 = (end *(end+1))/2;
+smallest = start<end?start:end;
+return Math.abs(n2 -n1)+smallest;
 };
 
 // Do not edit below this line
